f6d2946d8a4c02d26020fdbb08cbdd082e5523e8
felitas <felita.31895@gmail.com>
2016-12-15 16:19:51 +0700

    wehdeh

--- application/views/main.php
+++ application/views/main.php
@@ -23,7 +23,7 @@
     <link href="<?php echo base_url() ?>css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo base_url() ?>css/style.css">
     <link rel="stylesheet" href="<?php echo base_url() ?>css/animate.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Crimson+Text|Source+Sans+Pro" rel="stylesheet">
     
     <script src="<?php echo base_url() ?>js/jquery-1.12.4.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -33,9 +33,12 @@
     <script src="<?php echo base_url() ?>js/particles.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>js/jssor.slider-22.0.3.mini.js"></script>
     <style>
-        .parallax-window {
-            min-height: 400px;
-            background: transparent;
+        .logoFajar {
+            -webkit-transform: rotateX(150deg); /* Safari */
+            transform: rotateX(150deg);
+            position: absolute;
+            margin:auto;
+            left: 45%;
         }
         .navbar{
             margin-bottom: 0;
@@ -45,100 +48,50 @@
             margin-right: 50px;
             margin-left: 50px;
         }
+
+        .parallax-window {
+            min-height: 400px;
+            background: transparent;
+        }
+        
         #imgbutton2{
-            background: url(<?php echo base_url() ?>assets/log4.jpg);
+            background: url(<?php echo base_url() ?>assets/log3.jpg);
             background-size: cover;
         }
         #imgbutton3{
-            background: url(<?php echo base_url() ?>assets/jewel1.jpg);
+            background: url(<?php echo base_url() ?>assets/log5.jpg);
             background-size: cover;   
         }
         #imgbutton4{
-            background: url(<?php echo base_url() ?>assets/log5.jpeg);
+            background: url(<?php echo base_url() ?>assets/log1.jpg);
             background-size: cover;   
         }
         #imgbutton5{
-            background: url(<?php echo base_url() ?>assets/log6.jpg);
+            background: url(<?php echo base_url() ?>assets/log2.jpg);
             background-size: cover;   
         }
         #imgbutton6{
-            background: url(<?php echo base_url() ?>assets/log4.jpg);
+            background: url(<?php echo base_url() ?>assets/log8.jpg);
             background-size: cover;   
         }
         .hoverimg{
-            min-height: 380px;
+            min-height: 400px;
         }
         .hoverimg2{
-            min-height: 520px;
+            min-height: 650px;
         }
         .hoverimg3{
-            min-height: 255px;
+            min-height: 320px;
         }
-        /* jssor slider bullet navigator skin 05 css */
+        .carousel-control.left, .carousel-control.right {
+           background-image:none !important;
+           filter:none !important;
+        }
+        
         /*
-        .jssorb05 div           (normal)
-        .jssorb05 div:hover     (normal mouseover)
-        .jssorb05 .av           (active)
-        .jssorb05 .av:hover     (active mouseover)
-        .jssorb05 .dn           (mousedown)
-        */
-        .jssorb05 {
-            position: absolute;
-        }
-        .jssorb05 div, .jssorb05 div:hover, .jssorb05 .av {
-            position: absolute;
-            /* size of bullet elment */
-            width: 16px;
-            height: 16px;
-            background: url('<?php echo base_url() ?>img/b05.png') no-repeat;
-            overflow: hidden;
-            cursor: pointer;
-        }
-        .jssorb05 div { background-position: -7px -7px; }
-        .jssorb05 div:hover, .jssorb05 .av:hover { background-position: -37px -7px; }
-        .jssorb05 .av { background-position: -67px -7px; }
-        .jssorb05 .dn, .jssorb05 .dn:hover { background-position: -97px -7px; }
-
-        /* jssor slider arrow navigator skin 22 css */
-        /*
-        .jssora22l                  (normal)
-        .jssora22r                  (normal)
-        .jssora22l:hover            (normal mouseover)
-        .jssora22r:hover            (normal mouseover)
-        .jssora22l.jssora22ldn      (mousedown)
-        .jssora22r.jssora22rdn      (mousedown)
-        .jssora22l.jssora22lds      (disabled)
-        .jssora22r.jssora22rds      (disabled)
-        */
-        .jssora22l, .jssora22r {
-            display: block;
-            position: absolute;
-            /* size of arrow element */
-            width: 40px;
-            height: 58px;
-            cursor: pointer;
-            background: url('<?php echo base_url() ?>img/a22.png') center center no-repeat;
-            overflow: hidden;
-        }
-        .jssora22l { background-position: -10px -31px; }
-        .jssora22r { background-position: -70px -31px; }
-        .jssora22l:hover { background-position: -130px -31px; }
-        .jssora22r:hover { background-position: -190px -31px; }
-        .jssora22l.jssora22ldn { background-position: -250px -31px; }
-        .jssora22r.jssora22rdn { background-position: -310px -31px; }
-        .jssora22l.jssora22lds { background-position: -10px -31px; opacity: .3; pointer-events: none; }
-        .jssora22r.jssora22rds { background-position: -70px -31px; opacity: .3; pointer-events: none; }
-
-        .logoFajar {
-            -webkit-transform: rotateX(150deg); /* Safari */
-            transform: rotateX(150deg);
-            position: absolute;
-            margin:auto;
-            left: 45%;
-        }
         #infinitespin{
             position: absolute;
-            left: 48.8%;
+            left: 48.6%;
             top:5%;
             height: 80px;
             width: 80px;
@@ -154,20 +107,38 @@
             }
         }
         .rotating {
-            -webkit-animation: rotating 12s linear infinite;
+            -webkit-animation: rotating 10s linear 2;
+            visibility: hidden;
+        }*/
+        #social-media{
+            position: absolute;
+            right: 7px;
+            top: 7px;
+        }
+        .img{
+            display: inline;
+        }
+        .sosmed:hover img{
+            -webkit-filter: brightness(3); /* Safari 6.0 - 9.0 */
+            filter: brightness(3);
         }
 
     </style>
   </head>
   <body>
     <header id="particles-js" style="position: relative;">
-        <div id="infinitespin" class="rotating"></div>
-        <div class="animated flipInY logoFajar">
-            <img src="<?php echo base_url() ?>assets/FajarBaru.png" width="180" id="logo" height="180" class="img img-responsive"  alt="Logo Fajar Baru">    
-        </div>
-        
+            <div id="social-media" class="hidden-xs">
+                <a href="" class="sosmed"><img src="<?php echo base_url() ?>assets/iconig.png" height="25px" width="25px" class="img img-responsive"></a>
+                <a href="" class="sosmed"><img src="<?php echo base_url() ?>assets/iconfb.png" height="25px" width="25px" class="img img-responsive"></a>
+            </div>
+            <!-- <div id="infinitespin" class="rotating"></div> -->
+            <div class="animated flipInY logoFajar">
+                <img src="<?php echo base_url() ?>assets/FajarBaru.png" width="180" id="logo" height="180" class="img img-responsive"  alt="Logo Fajar baru">    
+            </div>    
 
     </header>
+
+    <!--Navbar-->
         <nav class="navbar navbar-default" id="navbar" style="padding-bottom: 0px !important;">
             <div class="container-fluid">
                 <!-- Brand and toggle get grouped for better mobile display -->
@@ -195,18 +166,6 @@
                         <li><a href="#">One more separated link</a></li>
                       </ul>
                     </li>
-                    <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gifts <span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">One more separated link</a></li>
-                      </ul>
-                    </li>
                     <li><a href="#">About Us </a></li>
                     <li><a href="#">Contact Us </a></li>
                   </ul>
@@ -214,46 +173,59 @@
                 </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid -->
         </nav>
-    
+ <!--Slider container-->   
 <div class="container-fluid" id="sliderfluid">
-<div class="row">
-    <!--SLIDER START-->
-    <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden; visibility: hidden;">
-        <!-- Loading Screen -->
-        <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
-            <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
-            <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
-        </div>
-        <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden;">
-            <div data-p="225.00">
-                <img data-u="image" src="<?php echo base_url() ?>assets/log4.jpg" />
+    <div class="row">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <img src="<?php echo base_url() ?>assets/Website5.jpg" alt="...">
+              <!-- <div class="carousel-caption">
+                ...
+              </div> -->
             </div>
-            <div data-p="225.00" style="display: none;">
-                <img data-u="image" src="<?php echo base_url() ?>assets/log5.jpeg" />
+            <div class="item">
+              <img src="<?php echo base_url() ?>assets/Website2.jpg" alt="...">
+              <!-- <div class="carousel-caption">
+                ...
+              </div> -->
             </div>
-            <a data-u="any" href="http://www.jssor.com" style="display:none">Full Width Slider</a>
-            <div data-p="225.00" data-po="80% 55%" style="display: none;">
-                <img data-u="image" src="<?php echo base_url() ?>assets/ring.jpg" />
+            <div class="item">
+              <img src="<?php echo base_url() ?>assets/Website1.jpg" alt="...">
+              <!-- <div class="carousel-caption">
+                ...
+              </div> -->
             </div>
+            <!-- ... -->
+          </div>
+
+          <!-- Controls -->
+          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
-        <!-- Bullet Navigator -->
-        <div data-u="navigator" class="jssorb05" style="bottom:16px;right:16px;" data-autocenter="1">
-            <!-- bullet navigator item prototype -->
-            <div data-u="prototype" style="width:16px;height:16px;"></div>
-        </div>
-        <!-- Arrow Navigator -->
-        <span data-u="arrowleft" class="jssora22l" style="top:0px;left:8px;width:40px;height:58px;" data-autocenter="2"></span>
-        <span data-u="arrowright" class="jssora22r" style="top:0px;right:8px;width:40px;height:58px;" data-autocenter="2"></span>
     </div>
-    <!-- #endregion Jssor Slider End -->
 </div>
-</div>
+<!--Content-->
 <div class="container-fluid">
-    <div class="row" style="padding-top: 20px;padding-bottom: 20px;">
+    <div class="row" style="padding-top: 10px;padding-bottom: 10px;">
         <div class="col-xs-5">
                 <div id="imgbutton1" class="hoverimg" style="position: relative;">
-                    <div style="position: absolute; color: #000; font-size: 20px; top: 120px; right:10px;text-align: right;">
-                        <h1>Fajar Baru</h1>
+                    <div style="position: absolute; color: #000; font-size: 20px; top: 120px; left:10px;text-align: left;">
+                        <h1 class="texthead">Fajar Baru</h1>
                         <h3>Jewellery Collection</h3>
                     </div>
                 </div>
@@ -262,6 +234,27 @@
             <div id="imgbutton2" class="hoverimg"></div>
         </div>
     </div>
+
+    <!-- <div class="row">
+        <div class="col-xs-12">
+            <div id="imgbutton6" class="hoverimg"></div>
+        </div>
+    </div>
+
+    <div class="row" style="padding-top: 10px;padding-bottom: 10px;">
+        <div class="col-xs-7">
+            <div id="imgbutton2" class="hoverimg"></div>
+                
+        </div>
+        <div class="col-xs-5" >
+            <div id="imgbutton1" class="hoverimg" style="position: relative;">
+                    <div style="position: absolute; color: #000; font-size: 20px; top: 120px; right:10px;text-align: right;">
+                        <h1 class="texthead">Fajar Baru</h1>
+                        <h3>Jewellery Collection</h3>
+                    </div>
+                </div>
+        </div>
+    </div> -->
 
     <div class="row">
         <div class="col-md-4">
@@ -272,7 +265,7 @@
             </div>
         </div>
         <div class="col-md-4">
-            <div class="row"  style="margin-bottom: 20px;">
+            <div class="row"  style="margin-bottom: 10px;">
                 <div class="col-xs-12">
                     <div id="imgbutton4" class="hoverimg3"></div>    
                 </div>
@@ -292,13 +285,18 @@
         </div>
     </div>
 </div>
-<div class="container-fluid">
 
+<!--Footer container-->
+<div class="container-fluid">
     <!-- <div class="row">
         <div class="parallax-window" data-parallax="scroll" data-image-src="<?php #echo base_url() ?>assets/ring.jpg" data-min-height="600px"></div>
     </div> -->
-    <footer style="height: 50px;"></footer>
+    <footer style="height: 50px;">
+        
+    </footer>
 </div>
+
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 
     <script type="text/javascript" src="<?php echo base_url() ?>js/main.js"></script>
